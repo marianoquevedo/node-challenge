42eaab62f32a468c97516b406b21f27885eee343
Mariano Quevedo <12384762+marianoquevedo@users.noreply.github.com>
2021-10-30 19:28:45 +0200

    fix: removeHeader for 'x-powered-by' not neded when using helmet

--- middleware/security.ts
+++ middleware/security.ts
@@ -1,5 +1,4 @@
 export default function security(req, res, next) {
-  res.removeHeader('X-Powered-By');
 
   if (decodeURIComponent(req.url).includes('<script>')) {
     return res.status(406).end('Illegal component in URI');
